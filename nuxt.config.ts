221b654d147e28ca528320ce9239f1914a281728
Vishnu Gupta <thenormvg@gmail.com>
2025-03-12 00:06:44 +0530

    is Device fixed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+
   app: {
     head: {
       title: "Cassette",
@@ -11,4 +12,6 @@ export default defineNuxtConfig({
       link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.svg" }],
     },
   },
+
+  modules: ["@nuxtjs/device"],
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,15 @@
 
 <script setup>
+// import { useDevice } from '@nuxtjs/device/runtime/composables/useDevice';
 
+const {isMobile}  = useDevice();
+console.log(isMobile,"is mobile");
 </script>
 
 <template>
 
-<Desktop v-if="$device.isDesktop" />
+<Desktop v-if="!isMobile"/>
+<!-- <Desktop v-if="$device.isDesktop" /> -->
 
 <Phone v-else/>"
 
